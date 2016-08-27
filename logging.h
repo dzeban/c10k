67cea7edc621b46f092ea829ac877b83d1001931
Alex Dzyoba <alex@dzyoba.com>
2016-08-27 13:32:48 +0300

    logging: report pid in logging messages

--- logging.h
+++ logging.h
@@ -2,11 +2,14 @@
 #define _LOGGING_H
 
 #include <stdio.h>
+#include <unistd.h>
 
 #ifdef DEBUG
-#define debug(M, ...) fprintf(stderr, "DEBUG %s:%d " M, __FILE__, __LINE__, ##__VA_ARGS__)
+#define debug(M, ...) fprintf(stderr, "=%05d= DEBUG %s:%d " M, getpid(), __FILE__, __LINE__, ##__VA_ARGS__)
 #else
 #define debug(M, ...)
 #endif
+
+#define info(M, ...) fprintf(stderr, "=%05d= INFO %s:%d " M, getpid(), __FILE__, __LINE__, ##__VA_ARGS__)
 
 #endif
