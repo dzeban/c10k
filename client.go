8ce307877bb7fad430faff85b18e88daecc64a86
Alex Dzyoba <alex@dzyoba.com>
2018-06-12 15:06:01 +0300

    Working c10k client

--- client.go
+++ client.go
@@ -5,22 +5,34 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"sync"
 	"time"
 )
 
-func main() {
+func request(wg *sync.WaitGroup) {
 	conn, err := net.Dial("tcp", "localhost:80")
 	if err != nil {
 		log.Fatal("dial error ", err)
 	}
 
-	fmt.Fprintf(conn, "GET /index.html HTTP/1.0\r\n\r\n")
+	time.Sleep(10 * time.Second)
 
-	time.Sleep(5 * time.Second)
-	body, err := bufio.NewReader(conn).ReadString('\n')
+	fmt.Fprintf(conn, "GET /index.html HTTP/1.1\r\n\r\n")
+
+	_, err = bufio.NewReader(conn).ReadString('\n')
 	if err != nil {
 		log.Fatal("read error ", err)
 	}
 
-	fmt.Printf(body)
+	wg.Done()
+}
+
+func main() {
+	var wg sync.WaitGroup
+	wg.Add(100)
+	for i := 0; i < 100; i++ {
+		go request(&wg)
+	}
+
+	wg.Wait()
 }
